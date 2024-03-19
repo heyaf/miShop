845c55c3bc09bc699ffb068c3952d16f9d640978
Hebert <Hebert@163.com>
2024-03-19 16:58:25 +0800

    fix bug

---
+++ ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/mac/Desktop/项目/mishop/ios/build/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
---
+++ ios/build/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
@@ -0,0 +1,20 @@
+dependencies: \
+  /Users/mac/Desktop/项目/mishop/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/mac/Desktop/项目/mishop/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/Flutter.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterEngineGroup.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/mac/Desktop/项目/mishop/ios/build/Debug-iphonesimulator/Flutter.framework/Headers/FlutterViewController.h
